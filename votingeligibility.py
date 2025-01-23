8f5a76de6df0f0b91becdf1a2d1d126015d99c9f
Juliewambui <juliewambui2021@gmail.com>
2025-01-23 23:16:19 +0300

    Add files via upload

---
+++ votingeligibility.py
@@ -0,0 +1,8 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:\Python\Python37\citizenship.py =================
+Are you an East African citizen? (yes/no): yes
+Enter your age: 33
+You are eligible to vote.
+>>> 
